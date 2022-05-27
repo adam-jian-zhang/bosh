c4480f3f17d858ea79e3a1a28202006184be265c
Daniel Felipe Ochoa <danielfelipo@vmware.com>
2022-05-27 08:37:34 -0500

    Improving nats sync unit tests.
    [#182098993] Upgrade NATS to 2.0
    Signed-off-by: Long Nguyen <nguyenlo@vmware.com>

--- src/bosh-nats-sync/lib/nats/nats_users_sync.rb
+++ src/bosh-nats-sync/lib/nats/nats_users_sync.rb
@@ -1,18 +1,19 @@
 require 'rest-client'
 require 'base64'
+require 'nats/nats_auth_config'
 
 module Nats
   class NatsUsersSync
-    def initialize(stdout, nats_config_file, bosh_config)
+    def initialize(stdout, nats_config_file_path, bosh_config)
       @stdout = stdout
-      @nats_config_file = nats_config_file
+      @nats_config_file_path = nats_config_file_path
       @bosh_config = bosh_config
     end
 
     def execute_nats_sync
       @stdout.puts 'Executing NATS Synchronization'
       vms_uuids = query_all_running_vms
-      # write_nats_config_file(vms_uuids)
+      write_nats_config_file(vms_uuids)
       @stdout.puts 'Finishing NATS Synchronization'
       vms_uuids
     end
@@ -20,8 +21,8 @@ module Nats
     private
 
     def call_bosh_api(endpoint)
-      response = RestClient.get @bosh_config.url + endpoint, 'Authentication:' => encode_basic_authentication
-      raise('Cannot access BOSH endpoint: ' + endpoint) unless response.code == 200
+      response = RestClient.get @bosh_config.url + endpoint, 'Authorization' => encode_basic_authentication
+      raise("Cannot access: #{endpoint}, Status Code: #{response.code}, #{response.body}") unless response.code == 200
 
       response.body
     end
@@ -32,8 +33,8 @@ module Nats
     end
 
     def get_vms_by_deployment(deployment)
-      deployments = JSON.parse(call_bosh_api('/deployments/' + deployment + '/vms'))
-      deployments.map { |deployment_obj| deployment_obj['agent_id'] }
+      virtual_machines = JSON.parse(call_bosh_api("/deployments/#{deployment}/vms"))
+      virtual_machines.map { |virtual_machine| virtual_machine['agent_id'] }
     end
 
     def query_all_running_vms
@@ -44,11 +45,13 @@ module Nats
     end
 
     def encode_basic_authentication
-      'Basic ' + Base64.encode64(@bosh_config.user + ':' + @bosh_config.password)
+      "Basic #{Base64.encode64("#{@bosh_config.user}:#{@bosh_config.password}")}"
     end
 
-    # def write_nats_config_file(vms_uuids)
-    #
-    # end
+    def write_nats_config_file(vms_uuids)
+      File.open(@nats_config_file_path, 'w') do |f|
+        f.write(JSON.unparse(NatsAuthConfig.new(vms_uuids).create_config))
+      end
+    end
   end
 end
--- src/bosh-nats-sync/spec/nats/nats_users_sync_spec.rb
+++ src/bosh-nats-sync/spec/nats/nats_users_sync_spec.rb
@@ -5,13 +5,14 @@ require 'nats/bosh_config'
 
 module Nats
   describe NatsUsersSync do
-    subject { NatsUsersSync.new(stdout, nats_config_file, bosh_config) }
+    subject { NatsUsersSync.new(stdout, nats_config_file_path, bosh_config) }
     let(:stdout) { StringIO.new }
-    let(:nats_config_file) { '/file1.json' }
+    let(:nats_config_file_path) { Tempfile.new('nats_config.json').path }
     let(:bosh_config) { BoshConfig.new(url, user, password) }
     let(:url) { 'http://127.0.0.1:25555' }
     let(:user) { 'admin' }
     let(:password) { 'admin' }
+    let(:ldap_user_name_base) { 'C=USA, O=Cloud Foundry, CN=%s.bosh-internal' }
     let(:deployments_json) do
       '[
   {
@@ -71,13 +72,40 @@ module Nats
       describe 'when there are deployments with running vms in Bosh' do
         before do
           stub_request(:get, url + '/deployments/deployment-1/vms')
+            .with(basic_auth: [user, password])
             .to_return(status: 200, body: vms_json)
           stub_request(:get, url + '/deployments')
+            .with(basic_auth: [user, password])
             .to_return(status: 200, body: deployments_json)
+          subject.execute_nats_sync
         end
 
-        it 'returns the vms belonging to the deployments' do
-          expect(subject.execute_nats_sync).to eq(%w[fef068d8-bbdd-46ff-b4a5-bf0838f918d9 c5e7c705-459e-41c0-b640-db32d8dc6e71])
+        it 'should write the right number of users to the NATs configuration file in the given path' do
+          file = File.read(nats_config_file_path)
+          data_hash = JSON.parse(file)
+          expect(data_hash['authorization']['users'].length).to eq(4)
+        end
+
+        it 'should write the right agent_ids to the NATs configuration file in the given path' do
+          file = File.read(nats_config_file_path)
+          data_hash = JSON.parse(file)
+          expect(data_hash['authorization']['users'])
+            .to include(include('user' => format(ldap_user_name_base, 'default.director')))
+          expect(data_hash['authorization']['users'])
+            .to include(include('user' => format(ldap_user_name_base, 'default.hm')))
+          expect(data_hash['authorization']['users'])
+            .to include(include('user' => format(ldap_user_name_base, 'fef068d8-bbdd-46ff-b4a5-bf0838f918d9.agent')))
+          expect(data_hash['authorization']['users'])
+            .to include(include('user' => format(ldap_user_name_base, 'c5e7c705-459e-41c0-b640-db32d8dc6e71.agent')))
+        end
+
+        it 'should not write the wrong ids to the NATs configuration file in the given path' do
+          file = File.read(nats_config_file_path)
+          data_hash = JSON.parse(file)
+          expect(data_hash['authorization']['users'])
+            .not_to include(include('user' => format(ldap_user_name_base, '9cb7120d-d817-40f5-9410-d2b6f01ba746.agent')))
+          expect(data_hash['authorization']['users'])
+            .not_to include(include('user' => format(ldap_user_name_base, '209b96c8-e482-43c7-9f3e-04de9f93c535.agent')))
         end
       end
     end
